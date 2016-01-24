348bf5194ff590134a2b0cd887e6e5bad29490c5
Josh C <legomania@cox.net>
2016-01-24 06:22:18 -0800

    Ive already made it...

--- Tapper/ViewController.swift
+++ Tapper/ViewController.swift
@@ -8,18 +8,88 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    //vars
+    var maxTaps = 0
+    var currentTaps = 0
+    
+    //outlets
+    @IBOutlet weak var playbtn: UIButton!
+    @IBOutlet weak var tapTxt: UITextField!
+    @IBOutlet weak var TapperLogo: UIImageView!
+    
+    @IBOutlet weak var coinbtn: UIButton!
+    @IBOutlet weak var tapsLbl: UILabel!
+    
+    
+    
+    //Actions
+    @IBAction func PlayBtnPressed(sender: AnyObject) {
+        
+        if tapTxt.text != "" && tapTxt.text != nil {
+            
+            maxTaps = Int(tapTxt.text!)!
+            currentTaps = 0
 
+            playbtn.hidden = true
+            tapTxt.hidden = true
+            TapperLogo.hidden = true
+            
+            coinbtn.hidden = false
+            tapsLbl.hidden = false
+            
+            updateTapsLbl()
+            
+            
+        }
+    }
+    
+    @IBAction func coinbtnPressed(sender: AnyObject) {
+        currentTaps++
+        updateTapsLbl()
+        
+        if isGameOver() {
+            restartGame()
+        }
+    }
+    //Functions
+    func restartGame() {
+        
+        tapTxt.text = nil
+        
+        playbtn.hidden = false
+        tapTxt.hidden = false
+        TapperLogo.hidden = false
+        
+        coinbtn.hidden = true
+        tapsLbl.hidden = true
+    }
+    
+    func updateTapsLbl() {
+        tapsLbl.text = "\(currentTaps) Taps"
+    }
+    
+    func isGameOver() -> Bool {
+        if currentTaps >= maxTaps {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.tapTxt.delegate = self;
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
     }
-
-
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 }
 
